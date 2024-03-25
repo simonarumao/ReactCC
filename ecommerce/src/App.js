999116105f41fd012676f03951703b2f6f88b603
aditigupta72003 <aditigupta72003@gmail.com>
2024-03-25 11:29:56 +0530

    updated

--- ecommerce/src/App.js
+++ ecommerce/src/App.js
@@ -2,28 +2,45 @@
 import React, {useState} from 'react';
 import './App.css';
 
+import Category from './components/Category';
+
 function App() {
   const [results, setResults] = useState([]);
   React.useEffect(() => {
     fetch("http://localhost:3001/categories")
       .then(response => response.json())
       .then(data => {
-        console.log(data);
-        setResults(data);
     })
   },[])
+
+  const renderCategories = () =>{
+
+    return results.map(c=>
+      <Category key={c.id} id={c.id} title={c.title}/>
+    );
+  }
+   
+  return (
+    <>
+      <header>My store </header>
+      <section>
+        <nav>
+          {
+          renderCategories()
+          }
+        </nav>
+      <article>
+        main area
+      </article>
+      </section>
+
+      <footer>
+        <article>main area</article>
+      </footer>
+    </>
+  );
   
 
-  return (
-    <div className="App">
-      {
-      results.map(d => (
-       <div key={d.id}>{d.title}</div>
-      ))
-      }
-     
-    </div>
-  );
 }
 
 export default App;
---
+++ ecommerce/src/components/Category.js
@@ -0,0 +1,8 @@
+// category.js
+import React from 'react';
+
+const Category = ({ id, title }) => {
+    return <div key={id}>{title}</div>;
+};
+
+export default Category;
